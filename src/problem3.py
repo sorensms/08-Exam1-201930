ff829306cdb9466b4b7f81d95f3a4109ffd20751
sorensms <sorensms@rose-hulman.edu>
2019-03-19 21:57:26 -0400

    Done

--- src/problem3.py
+++ src/problem3.py
@@ -3,8 +3,8 @@ Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Maddie Sorensen.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
+# Done: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
 # functions defined below.  They are the same as what you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -170,6 +170,12 @@ def run_test_problem3a():
 
 
 def problem3a(a, b):
+    count=0
+    for k in range (a,a**b):
+        if is_prime(k) & is_prime(sum_of_digits(k)):
+            count=count+k
+    return count
+
     """
     What comes in:  Positive integers a and b, with a >= 2.
     What goes out:
@@ -190,7 +196,7 @@ def problem3a(a, b):
       -- problem3a(8, 2) = 11 + 23 + 29 + 41 + 43 + 47 + 61  (which is 255)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -257,6 +263,17 @@ def run_test_problem3b():
 
 
 def problem3b(m, x):
+    count=0
+    for k in range (m**2,m**4):
+        if is_prime(k) & is_prime(sum_of_digits(k)):
+            count=count+1
+
+        if count>=x:
+            return True
+        else:
+            return False
+
+
     """
     What comes in:  A positive integer m >= 2, and an integer x.
     What goes out:
